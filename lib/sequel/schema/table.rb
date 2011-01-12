d3e4f049adeb412d8a2ce97db9f6456664d3ede5
Roland Swingler <roland.swingler@gmail.com>
2011-01-12 18:14:28 +0000

    Documentation and make table names symbols.

--- lib/sequel/schema/table.rb
+++ lib/sequel/schema/table.rb
@@ -1,16 +1,21 @@
 module Sequel
   module Schema
     class Table
+      # The name of this table
       attr_accessor :name
 
+      # The columns of this table
       attr_reader :columns
 
+      # The non-primary key indexes of this table
       attr_reader :indexes
 
+      # Any table options.
       attr_reader :options
       
+      # Creates a new table definition
       def initialize(name, options={})
-        @name = name
+        @name = name.to_sym
         @options = options
         @columns = []
         @indexes = []
--- spec/table_spec.rb
+++ spec/table_spec.rb
@@ -2,7 +2,7 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe Sequel::Schema::Table do
   it "should have a name" do
-    Sequel::Schema::Table.new("example").name.should == "example"
+    Sequel::Schema::Table.new("example").name.should == :example
   end
 
   it "can have arbitrary table options" do
